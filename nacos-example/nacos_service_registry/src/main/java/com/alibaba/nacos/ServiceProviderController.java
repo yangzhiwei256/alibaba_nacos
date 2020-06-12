7dca7f71ce217696dd67c95e32da2ca5f36ae37b
zhiwei_yang <yangzhiwei1488@foxmail.com>
2020-06-12 16:31:21 +0800

    完善nacos example测试接口

--- nacos-example/nacos_service_registry/src/main/java/com/alibaba/nacos/ServiceProviderController.java
+++ nacos-example/nacos_service_registry/src/main/java/com/alibaba/nacos/ServiceProviderController.java
@@ -1,7 +1,17 @@
 package com.alibaba.nacos;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.nacos.client.naming.utils.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author zhiwei_yang
@@ -10,8 +20,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ServiceProviderController {
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/provider")
     public String provider(){
         return "This is Service Provider";
+    }
+
+    @GetMapping("/service")
+    public String service(){
+        List<String> serviceIds = discoveryClient.getServices();
+        if(CollectionUtils.isEmpty(serviceIds)){
+            return StringUtils.EMPTY;
+        }
+        Map<String, List<ServiceInstance>> serviceInstanceMap = new HashMap<>(serviceIds.size());
+        serviceIds.forEach(serviceId -> serviceInstanceMap.putIfAbsent(serviceId, discoveryClient.getInstances(serviceId))
+        );
+        return JSON.toJSONString(serviceInstanceMap);
     }
 }
